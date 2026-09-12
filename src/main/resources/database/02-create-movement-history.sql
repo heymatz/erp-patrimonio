0a70ba6d0da22f86b92164ada72b4d73291f0ce8
heymatz <matheus.henrique200598@gmail.com>
2026-09-12 18:49:06 -0300

    fix(db): create historico_movimentacao table to resolve failed tests

--- src/main/resources/database/02-create-movement-history.sql
+++ src/main/resources/database/02-create-movement-history.sql
@@ -1,11 +1,13 @@
+-- Seleciona o banco de dados erp_patrimonio para criar a tabela
+USE erp_patrimonio; 
+
 CREATE TABLE historico_movimentacao (
     id INT AUTO_INCREMENT PRIMARY KEY,
     patrimonio_id INT NOT NULL,
     local_origem_id INT NOT NULL,
     local_destino_id INT NOT NULL,
-    data_movimentacao DATETIME NOT NULL,
+    data_movimentacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     motivo VARCHAR(255),
-    
     FOREIGN KEY (patrimonio_id) REFERENCES patrimonios(id),
     FOREIGN KEY (local_origem_id) REFERENCES locais(id),
     FOREIGN KEY (local_destino_id) REFERENCES locais(id)
